0b6b3a1fead1950bd8d66fc13edbd4ffff605541
OCETP <pk@OCETP.local>
2022-05-25 21:59:21 +0300

    {commit}

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
+import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Post from './components/post';
-import initialData from './initialData';
 import Form from './components/form';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        posts: [], 
+        isLoading: true,
+    }
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(data => this.setState({
+          posts: data,
+          isLoading: false,
+      }))
+      .catch(error => console.error(error))
+  } 
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <h1>Loading</h1>
+      )
+    }
+    return (
       <>
         <Header/>
-        {initialData.map(post => <Post post={post} numb={10} key={post.id} />)}
+        {this.state.posts.map(post => <Post post={post} numb={10} key={post.id} />)}
         <Form/>
       </>
-  )
+    )
+  }
 }
+
 
 export default App;
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import './index.css';
 
-const Form = () => {
-    return (
-        <div className="formWrapper">
-            <input type="text" className="formInput" id='formTitle' placeholder="Заголовок поста"/>
-            <textarea className="formInput" id="formText" placeholder="Текст поста"></textarea>
-            <input type="text" className="formInput" id='formImg' placeholder="Ссылка на изображение"/>
-            <input type="text" className="formInput" id="formSpotify" placeholder="Ссылка Spotify"/>
-            <input type="text" className="formInput" id="formApple" placeholder="Ссылка Apple"/>
-            <label className="checkboxCave">
-                <input id='formCheckbox' type="checkbox"/>
-                <span> подстветить</span>
-            </label>
-        <button className="add-post" onclick='addPost()'>Add post</button>
-    </div>
-    )
-};
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            description: '',
+            cover: '',
+            apple: '',
+            spotify: '',
+            highlighted: false,
+        }
+    }
+    handleSubmit = () => {
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div className="formWrapper">
+                <input type="text" className="formInput" id='formTitle' placeholder="Заголовок поста" value={this.state.title} onChange={event => this.setState({title: event.target.value})}/>
+                <textarea className="formInput" id="formText" placeholder="Текст поста" value={this.state.description} onChange={event => this.setState({description: event.target.value})}></textarea>
+                <input type="text" className="formInput" id='formImg' placeholder="Ссылка на изображение" value={this.state.cover} onChange={event => this.setState({cover: event.target.value})}/>
+                <input type="text" className="formInput" id="formSpotify" placeholder="Ссылка Spotify" value={this.state.spotify} onChange={event => this.setState({spotify: event.target.value})}/>
+                <input type="text" className="formInput" id="formApple" placeholder="Ссылка Apple" value={this.state.apple} onChange={event => this.setState({apple: event.target.value})}/>
+                <label className="checkboxCave" >
+                    <input id='formCheckbox' type="checkbox" value={this.state.highlighted} onChange={event => this.setState({highlighted: event.target.checked})}/>
+                    <span> подсветить</span>
+                </label>
+            <button className="add-post" onClick={this.handleSubmit}>Add post</button>
+        </div>
+        )  
+    }
+}
 
 export default Form;
