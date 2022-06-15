4d9b5e15b3e7328c4816e9504a545b613267df83
OCETP <pk@OCETP.local>
2022-06-15 23:11:27 +0300

    valid checkFields

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import './index.css';
 import Button from "../../ui/button";
+import cx from "classnames";
 
 
 class Form extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            title: '',
-            description: '',
-            cover: '',
-            apple: '',
-            spotify: '',
-            highlighted: false,
+          title: '',
+          description: '',
+          cover: '',
+          apple: '',
+          spotify: '',
+          highlighted: false,
+          errors: {
+            title: false,
+            description: false,
+            cover: false,
+            apple: false,
+            spotify: false,
+          }
         }
     }
     clearFields = () => {
@@ -25,7 +33,10 @@ class Form extends React.Component {
             highlighted: false,
         })
     }
-    onSubmit = () => {
+    onSubmit = async () => {
+        if (await this.checkFields() === false) {
+            return
+        }
         const post = {
             title: this.state.title,
             description: this.state.description,
@@ -37,15 +48,32 @@ class Form extends React.Component {
         }
         this.props.handleSubmit(post)
         this.clearFields()
+    
+    }
+    checkFields = async () => {
+        return new Promise(resolve => {
+          this.setState({
+            errors: {
+              title: !this.state.title,
+              description: !this.state.description,
+              cover: !this.state.cover,
+              apple: !this.state.apple,
+              spotify: !this.state.spotify
+            }
+          }, () => resolve(!Object.values(this.state.errors).some(error => Boolean(error))))
+        })
+      }
+    handleChangeField = (event, fieldName) => {
+        this.setState({[fieldName]: event.target.value, errors: {...this.state.errors, [fieldName]: false}})
     }
     render() {
         return (
             <div className="formWrapper">
-                <input type="text" className="formInput" id='formTitle' placeholder="Заголовок поста" value={this.state.title} onChange={event => this.setState({title: event.target.value})}/>
-                <textarea className="formInput" id="formText" placeholder="Текст поста" value={this.state.description} onChange={event => this.setState({description: event.target.value})}></textarea>
-                <input type="text" className="formInput" id='formImg' placeholder="Ссылка на изображение" value={this.state.cover} onChange={event => this.setState({cover: event.target.value})}/>
-                <input type="text" className="formInput" id="formSpotify" placeholder="Ссылка Spotify" value={this.state.spotify} onChange={event => this.setState({spotify: event.target.value})}/>
-                <input type="text" className="formInput" id="formApple" placeholder="Ссылка Apple" value={this.state.apple} onChange={event => this.setState({apple: event.target.value})}/>
+                <input type="text" className={cx("formInput", this.state.errors.title && "error")} id='formTitle' placeholder="Заголовок поста" value={this.state.title} onChange={event => this.handleChangeField(event, 'title')}/>
+                <textarea className={cx("formInput", this.state.errors.description && "error")} id="formText" placeholder="Текст поста" value={this.state.description} onChange={event => this.handleChangeField(event, 'description')}></textarea>
+                <input type="text" className={cx("formInput", this.state.errors.cover && "error")} id='formImg' placeholder="Ссылка на изображение" value={this.state.cover} onChange={event => this.handleChangeField(event, 'cover')}/>
+                <input type="text" className={cx("formInput", this.state.errors.spotify && "error")} id="formSpotify" placeholder="Ссылка Spotify" value={this.state.spotify} onChange={event => this.handleChangeField(event, 'spotify')}/>
+                <input type="text" className={cx("formInput", this.state.errors.apple && "error")} id="formApple" placeholder="Ссылка Apple" value={this.state.apple} onChange={event => this.handleChangeField(event, 'apple')}/>
                 <label className="checkboxCave" >
                     <input id='formCheckbox' type="checkbox" value={this.state.highlighted} onChange={event => this.setState({highlighted: event.target.checked})}/>
                     <span> подсветить</span>
